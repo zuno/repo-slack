a628542f53091af391d8ba9497166990dadc6014
Matteo Rossini <zerouno@slacky.it>
2012-08-03 23:12:40 +0200

    Fri Aug  3 13:12:30 CEST 2012 -----------------------------         new file:   openjdk/openjdk.SlackBuild         new file:   xerces/xerces.SlackBuild         deleted:    jdk/jdk.SlackBuild         deleted:    jre/jre.SlackBuild

---
+++ slackware64-current/openjdk/doinst.sh
@@ -0,0 +1,28 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/java/jvm.cfg.new
+config etc/java/java.policy.new
+config etc/java/java.security.new
+config etc/java/nss.cfg.new
+
+if [ -x usr/bin/update-desktop-database ]; then
+  chroot . /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x usr/bin/gtk-update-icon-cache ]; then
+    chroot . /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
